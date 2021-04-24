a6240ccf5601b1c7da694f2df644953626dffe48
dungxtd <dungxtd20@gmail.com>
2021-04-24 08:12:50 +0700

    second commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const selected = document.querySelector(".icon");
+/*const selected = document.querySelector(".icon");
 const input = document.querySelector("#input-gender");
 const optionsContainer = document.querySelector(".options-container");
 
@@ -10,10 +10,34 @@ selected.addEventListener("click", () => {
 
 optionsList.forEach(e => {
     e.addEventListener("click", () => {
-
         input.value = e.querySelector("label").innerHTML;
         optionsContainer.classList.remove("active");
     });
 });
 
+useEffect(() => {
+    //  thêm khi đã mount
+    document.addEventListener("mousedown", handleClick);
+    // dọn dẹp
+    return () => {
+        document.removeEventListener("mousedown", handleClick);
+    }
+}, [])
 
+const handleClick = e => {
+    if (node.current.contains(e.target)) {
+        return;
+    }
+
+};*/
+
+$(function () {
+    $('.icon').click(function () {
+        $('.options-container').toggleClass('active');
+    });
+
+    $(".option").on('click', function () {
+        $('#input-gender').val(this.innerText);
+        $('.options-container').removeClass('active');
+    })
+});
